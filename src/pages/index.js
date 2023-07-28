c2234c869b010197278113475884fb9bebcd62cb
alifsakib <alifsakib@gmail.com>
2023-07-28 16:15:05 +0600

    created root layout layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <h1>Hello, world!</h1>;
+import RootLayout from "@/components/layout/root-layout";
+
+const HomePage = () => {
+  return <h1>Home Page</h1>;
 };
 
-export default Home;
+export default HomePage;
+
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
